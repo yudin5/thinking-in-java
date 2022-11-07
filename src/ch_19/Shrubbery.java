a8c633252ea6b654d8ca41ec7573532dc66920cb
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2022-11-07 22:02:21 +0300

    shrubbery test, initial

---
+++ src/ch_19/Shrubbery.java
@@ -0,0 +1,7 @@
+package ch_19;
+
+public enum Shrubbery {
+    TASTE,
+    COLLY,
+    FLAG
+}
---
+++ src/ch_19/ShrubberyDrive.java
@@ -0,0 +1,11 @@
+package ch_19;
+
+public class ShrubberyDrive {
+    public static void main(String[] args) {
+        for (Shrubbery s : Shrubbery.values()) {
+            System.out.println("s = " + s);
+            System.out.println(s.ordinal());
+            System.out.println(s.getDeclaringClass().getSimpleName());
+        }
+    }
+}
